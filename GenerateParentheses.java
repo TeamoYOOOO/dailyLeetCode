a21bce699bff94c22e6c28b5edd70de87b7df702
Ying <zy414371666@gmail.com>
2021-02-20 00:02:51 -0800

    22. Generate Parentheses

--- GenerateParentheses.java
+++ GenerateParentheses.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParentheses {
   List<String> res = new ArrayList<>();
   public List<String> generateParenthesis(int n) {
