4309c224e3bf4ae2772d8365db1447459b692eb1
Ying <zy414371666@gmail.com>
2021-03-21 23:40:22 -0700

    1802. Maximum Value at a Given Index in a Bounded Array

---
+++ MaximumValueAtaGivenIndexInaBoundedArray.java
@@ -0,0 +1,36 @@
+public class MaximumValueAtaGivenIndexInaBoundedArray {
+  public int maxValue(int n, int index, int maxSum) {
+    int left = 1;
+    int right = maxSum;
+    int ans = 1;
+
+    while(left <= right){
+      int mid = (left + right) / 2;
+      boolean exceed = check(n, index, maxSum, mid);
+      if (exceed) {
+        right = mid - 1;
+      } else {
+        ans = mid;
+        left = mid + 1;
+      }
+    }
+    return ans;
+  }
+
+  private long getSumInRange(long peak, int length) {
+    long sum = 0;
+    if (length > peak) {
+      sum += (1 + peak) * peak / 2;
+      sum += (length - peak);
+    } else {
+      long minimum = peak - length + 1;
+      sum += (minimum + peak) * length / 2;
+    }
+    return sum;
+  }
+
+  private boolean check(int n, int index, int maxSum, int peak) {
+    long sum = getSumInRange(peak, index + 1) + getSumInRange(peak, n - index) - peak;
+    return sum > (long)maxSum;
+  }
+}
