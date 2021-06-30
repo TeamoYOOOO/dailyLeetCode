be1e0a35593f26ad88c80ef8d9a93c00f0ce89e2
Ying <zy414371666@gmail.com>
2021-06-30 01:24:21 -0700

    287. Find the Duplicate Number

--- FindTheDuplicateNumber.java
+++ FindTheDuplicateNumber.java
@@ -11,24 +11,35 @@ public class FindTheDuplicateNumber {
     // }
     // return num;
 
-    Arrays.sort(nums);
-    int left = 0, right = nums.length-1;
-    while(left < right){
-      int mid = left + (right - left)/2;
-      int count = 0;
-      for(int i : nums){
-        if(i <= mid) count++;
-      }
-      if(count<= mid){
-        left = mid + 1;
-      }
-      else{
-        right = mid;
-      }
-    }
-    return left;
+
+    //竟然真的可以。。那还BS个啥注意不能检查nums[x] != x+1 因为有些数可能没有 case: [7,9,7,4,2,8,7,7,1,5] 但是重复的数一定会补刀x对应nums[x] 因为占了多的那个1的坑
+    // Arrays.sort(nums);
+    // for(int x = 0; x < nums.length; x++){
+    //     if(nums[x] == x) return nums[x];
+    // }
+    // return -1;
+
+
+    //---------------Binary Search-----------
+    // Arrays.sort(nums);
+    // int left = 0, right = nums.length-1;
+    // while(left < right){
+    //     int mid = left + (right - left)/2;
+    //     int count = 0;
+    //     for(int i : nums){
+    //         if(i <= mid) count++;
+    //     }
+    //     if(count<= mid){
+    //         left = mid + 1;
+    //     }
+    //     else{
+    //         right = mid;
+    //     }
+    // }
+    // return left;
   }
 
+  //-----------fast slow pointer--------
 //     public int findDuplicate(int[] nums) {
 //         int slow = nums[0];
 // 		int fast = nums[nums[0]];
@@ -36,6 +47,7 @@ public class FindTheDuplicateNumber {
 // 			slow = nums[slow];
 // 			fast = nums[nums[fast]];
 // 		}
+
 // 		fast = 0;
 // 		while (fast != slow){
 // 			fast = nums[fast];
